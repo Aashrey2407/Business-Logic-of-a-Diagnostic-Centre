f6c9d900b620b276452c137060c741ac6c045f06
Abdul-Rahman-Yakoob <167309882+Abdul-Rahman-Yakoob@users.noreply.github.com>
2024-04-21 00:05:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main extends JFrame{
-    JButton b1,b2;
+    JButton b1,b2,b3;
     public Main(){
         this.setTitle("ABC Diagonostic Centre");
         this.setLayout(null);
@@ -11,10 +11,13 @@ public class Main extends JFrame{
         this.setBounds(300,300,500,500);
         b1=new JButton("Sign Up");
         b2=new JButton("Login");
-        b1.setBounds(200,146,100,55);
-        b2.setBounds(200,220,100,55);
+        b3=new JButton("Admin Dashboard");
+        b1.setBounds(200,126,100,55);
+        b2.setBounds(200,200,100,55);
+        b3.setBounds(157,274,200,55);
         this.add(b1);
         this.add(b2);
+        this.add(b3);
         b1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -28,6 +31,12 @@ public class Main extends JFrame{
             }
         });
         this.setVisible(true);
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new Admin();
+            }
+        });
     }
 
     public static void main(String[] args) {
